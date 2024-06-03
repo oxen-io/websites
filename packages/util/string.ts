4f47a9583624f3f4cb1865441b692015e3c0059a
Ryan Miller <ryan.m@getsession.org>
2024-06-03 16:31:33 +1000

    Update string.ts

--- packages/util/string.ts
+++ packages/util/string.ts
@@ -1,11 +1,11 @@
 /**
  * Collapses a string by replacing characters between a specified length with an ellipsis.
- * The final length of the string will be the {endLengths} * 2 + 3.
+ * The final length of the string will be the {sideLength} * 2 + 3.
  * @param str - The input string to collapse.
- * @param endLengths - The number of characters to keep on either side of the string. Default is 4.
+ * @param sideLength - The number of characters to keep on either side of the string. Default is 4.
  * @returns The collapsed string.
  */
-export const collapseString = (str: string, endLengths = 4): string => {
-  if (str.length <= endLengths * 2 + 3) return str;
-  return `${str.slice(0, endLengths)}...${str.slice(-endLengths)}`;
+export const collapseString = (str: string, sideLength = 4): string => {
+  if (str.length <= sideLength * 2 + 3) return str;
+  return `${str.slice(0, sideLength)}...${str.slice(-sideLength)}`;
 };
