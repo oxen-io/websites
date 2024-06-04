e303cc66f5aab43489e4a2d5b0981fa79f8944f2
Ryan Miller <ryan.m@getsession.org>
2024-06-04 12:22:10 +1000

    chore: rename default button size to md

--- packages/ui/components/ui/button.tsx
+++ packages/ui/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'px-4 py-2',
+        md: 'px-4 py-2',
         sm: 'h-9 px-3',
         lg: 'h-11 px-8',
         icon: 'h-10 w-10',
@@ -27,7 +27,7 @@ const buttonVariants = cva(
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size: 'md',
     },
   }
 );
