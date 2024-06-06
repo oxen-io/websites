d6487f2152fcd4e27cdb3cf3c9ad2796d9b82523
Ryan Miller <ryan.m@getsession.org>
2024-06-06 11:58:34 +1000

    fix: update locales and data test ids

--- apps/staking/testing/data-test-ids.tsx
+++ apps/staking/testing/data-test-ids.tsx
@@ -4,6 +4,7 @@ export enum ButtonDataTestId {
   Home_Run_Node = 'button:how-to-run-a-session-node',
   Not_Found_Return_Home = 'button:not-found-return-home',
   Copy_Node_Id_To_Clipboard = 'button:copy-node-id-to-clipboard',
+  Node_Card_Stake = 'button:node-card-stake',
 }
 
 export enum ModuleDataTestId {
@@ -11,4 +12,23 @@ export enum ModuleDataTestId {
   Claim_Tokens = 'module:claim-tokens',
   Unclaimed_Tokens = 'module:unclaimed-tokens',
   Daily_Rewards = 'module:daily-rewards',
+}
+
+export enum NodeCardDataTestId {
+  Staked_Node = 'node-card:staked-node',
+  Open_Node_Card = 'node-card:open-node-card',
+}
+
+export enum StakedNodeDateTestId {
+  Staked_Node_Card_Header = 'staked-node-card:staked-node-header',
+  Staked_Node_Card_Text = 'staked-node-card:staked-node-text',
+  Staked_Node_Card_Title = 'staked-node-card:staked-node-title',
+  Staked_Node_Card_Pub_Key = 'staked-node-card:staked-node-pub-key',
+}
+
+export enum OpenNodeDataTestId {
+  Open_Node_Card_Header = 'staked-node-card:staked-node-header',
+  Open_Node_Card_Title = 'open-node-card:open-node-card-title',
+  Open_Node_Card_Text = 'open-node-card:open-node-card-text',
+  Open_Node_Card_Pub_Key = 'open-node-card:open-node-card-pub-key',
 }
