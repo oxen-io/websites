b801859c466634c4111eb22a8dc8a86022f95f66
Ryan Miller <ryan.m@getsession.org>
2024-06-25 11:34:40 +1000

    fix: update open nodes layout

--- apps/staking/app/stake/page.tsx
+++ apps/staking/app/stake/page.tsx
@@ -1,33 +1,18 @@
-import {
-  Module,
-  ModuleContent,
-  ModuleHeader,
-  ModuleText,
-  ModuleTitle,
-} from '@session/ui/components/Module';
-import { ModuleGrid, ModuleGridHeader, ModuleGridTitle } from '@session/ui/components/ModuleGrid';
-import { useTranslations } from 'next-intl';
-import OpenNodes from './OpenNodes';
+import { ModuleGrid } from '@session/ui/components/ModuleGrid';
+import PriceModule from '../mystakes/modules/PriceModule';
+import OpenNodesModule from './OpenNodes';
 
 export default function Page() {
-  const dictionary = useTranslations('stakeNow');
   return (
-    <ModuleGrid size="lg" className="h-full w-screen px-10 py-6">
-      <ModuleGrid variant="section" colSpan={1}>
-        <Module size="lg" className="hidden h-full lg:flex">
-          <ModuleHeader variant="overlay">
-            <ModuleTitle>Price</ModuleTitle>
-            <ModuleText>$XX.XX USD</ModuleText>
-          </ModuleHeader>
-          <ModuleContent className="h-[300px]"></ModuleContent>
-        </Module>
-      </ModuleGrid>
-      <ModuleGrid variant="section" colSpan={2} className="h-full">
-        <ModuleGridHeader>
-          <ModuleGridTitle>{dictionary('title')}</ModuleGridTitle>
-        </ModuleGridHeader>
-        <OpenNodes />
-      </ModuleGrid>
+    <ModuleGrid size="lg" className="h-full px-4 md:auto-rows-auto md:px-10">
+      <div className="md:max-h-screen-minus-header col-span-1 flex h-full min-h-max flex-col gap-4 py-6 md:overflow-y-auto md:overflow-x-hidden">
+        <PriceModule />
+      </div>
+      <div className="md:max-h-screen-minus-header col-span-2 h-full py-6">
+        <ModuleGrid variant="section" colSpan={2} className="h-full">
+          <OpenNodesModule />
+        </ModuleGrid>
+      </div>
     </ModuleGrid>
   );
 }
