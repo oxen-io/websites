aad3886e675e213529d31f293a7e206734b1a9c3
Ryan Miller <ryan.m@getsession.org>
2024-06-27 16:33:25 +1000

    fix: update open node fetch to use backend

--- apps/staking/app/stake/OpenNodes.tsx
+++ apps/staking/app/stake/OpenNodes.tsx
@@ -1,50 +1,15 @@
 'use client';
-import { OpenNode, OpenNodeCard } from '@/components/OpenNodeCard';
+
+import { OpenNodeCard } from '@/components/OpenNodeCard';
+import { useSessionStakingQuery } from '@/providers/sent-staking-provider';
 import {
   ModuleGridContent,
   ModuleGridHeader,
   ModuleGridTitle,
 } from '@session/ui/components/ModuleGrid';
+import { Loading } from '@session/ui/components/loading';
+import { useWallet } from '@session/wallet/hooks/wallet-hooks';
 import { useTranslations } from 'next-intl';
-
-const openNodes: Array<OpenNode> = [
-  {
-    pubKey:
-      'LCys52dasGmZxo5uC9smEKJHeNGQgc6FU4UtfxXbK7u4H3asfL5dRfoRBajnuuQdEhXjHggCtMTfA6BSoT8eb3G7Fd6p4',
-    maxContribution: 1000,
-    minContribution: 100,
-    operatorFee: 0.1,
-  },
-
-  {
-    pubKey:
-      'LCyJd2dFcGmZsz5uC9smEKJHeNGQgc6FU4UtfxXbK7u4H3asfL5dRfoRBajnuuQdEhXjHggCtMTfA6BSoT8eb3G7Fd6p5',
-    maxContribution: 1000,
-    minContribution: 100,
-    operatorFee: 0.1,
-  },
-  {
-    pubKey:
-      'LCya52dFcGmZxo5uC9smEKJHeNGQgc6FU4UtfxXbK7u4H3asfL5dRfoRBajnuuQdEhXjHggCtMTfA6BSoT8eb3G7Fd6p6',
-    maxContribution: 1000,
-    minContribution: 100,
-    operatorFee: 0.1,
-  },
-  {
-    pubKey:
-      'LCyJg2dFcGmZxo5uC9smEKJHeNGQgc6FU4UtfxXbK7u4H3asfL5dRfoRBajnuuQdEhXjHggCtMTfA6BSoT8eb3G7Fd6p7',
-    maxContribution: 1000,
-    minContribution: 100,
-    operatorFee: 0.1,
-  },
-  {
-    pubKey:
-      'LCyJ5bdFcGmZxo5uC9smEKJHeNGQgc6FU4UtfxXbK7u4H3asfL5dRfoRBajnuuQdEhXjHggCtMTfA6BSoT8eb3G7Fd6p8',
-    maxContribution: 1000,
-    minContribution: 100,
-    operatorFee: 0.1,
-  },
-];
 
 export default function OpenNodesModule() {
   const dictionary = useTranslations('modules.openNodes');
@@ -59,9 +24,16 @@ export default function OpenNodesModule() {
 }
 
 function OpenNodes() {
-  return (
+  const { address } = useWallet();
+  const { data, isLoading } = useSessionStakingQuery({
+    query: 'getOpenNodes',
+    args: { userAddress: address! },
+  });
+  return isLoading || !data ? (
+    <Loading />
+  ) : (
     <ModuleGridContent className="h-full md:overflow-y-auto">
-      {openNodes.map((node) => (
+      {data.nodes.map((node) => (
         <OpenNodeCard key={node.pubKey} node={node} />
       ))}
     </ModuleGridContent>
