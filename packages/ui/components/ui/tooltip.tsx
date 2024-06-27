f34a09fed47b4a51fd2506d18e3ecbb400da4075
Ryan Miller <ryan.m@getsession.org>
2024-06-27 16:30:05 +1000

    feat: create tooltip

--- packages/ui/components/ui/tooltip.tsx
+++ packages/ui/components/ui/tooltip.tsx
@@ -1,30 +1,34 @@
 'use client';
 
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
 import * as React from 'react';
 
 import { cn } from '../../lib/utils';
 
-const TooltipProvider = TooltipPrimitive.Provider;
+const Tooltip = PopoverPrimitive.Root;
 
-const Tooltip = TooltipPrimitive.Root;
-
-const TooltipTrigger = TooltipPrimitive.Trigger;
+const TooltipTrigger = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <PopoverPrimitive.Trigger ref={ref} className={cn('cursor-pointer', className)} {...props} />
+));
 
 const TooltipContent = React.forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <TooltipPrimitive.Content
+  <PopoverPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
+    side="top"
     className={cn(
-      'text-session-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-gray-dark z-50 overflow-hidden rounded-full bg-opacity-80 px-3 py-1.5 text-sm shadow-xl',
+      'text-session-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-session-black border-px z-50 overflow-hidden rounded-full border border-[#1C2624] bg-opacity-50 px-4 py-2 text-sm shadow-xl outline-none',
       className
     )}
     {...props}
-  ></TooltipPrimitive.Content>
+  ></PopoverPrimitive.Content>
 ));
-TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+TooltipContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
+export { Tooltip, TooltipContent, TooltipTrigger };
