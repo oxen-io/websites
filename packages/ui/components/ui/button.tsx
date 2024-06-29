739890b85128775deac5fa3d0ba933ff4fe2a136
Ryan Miller <ryan.m@getsession.org>
2024-06-29 15:19:03 +1000

    feat: add user defined separator string

--- packages/ui/components/ui/button.tsx
+++ packages/ui/components/ui/button.tsx
@@ -19,8 +19,8 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        md: 'px-4 py-2',
         sm: 'h-9 px-3',
+        md: 'px-4 py-2',
         lg: 'h-11 px-8',
         icon: 'h-10 w-10',
       },
