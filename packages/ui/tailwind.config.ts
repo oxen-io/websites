41b55ef8a60039290145f6745cdbe9be70f759f1
Ryan Miller <ryan.m@getsession.org>
2024-06-29 10:19:33 +1000

    feat: add info colour

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -102,6 +102,10 @@ export default {
           DEFAULT: 'hsl(var(--warning))',
           foreground: 'hsl(var(--warning-foreground))',
         },
+        info: {
+          DEFAULT: 'hsl(var(--info))',
+          foreground: 'hsl(var(--info-foreground))',
+        },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
           foreground: 'hsl(var(--muted-foreground))',
