9d7f0645b022d2ee15cc24a127148801d57565f9
Ryan Miller <ryan.m@getsession.org>
2024-07-02 09:32:06 +1000

    feat: add external link out icon

--- apps/staking/app/stake/node/[nodeId]/page.tsx
+++ apps/staking/app/stake/node/[nodeId]/page.tsx
@@ -1,3 +1,4 @@
+import { LinkOutIcon } from '@session/ui/icons/LinkOutIcon';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import ActionModule from '../../ActionModule';
@@ -16,7 +17,14 @@ export default function NodePage({ params }: NodePageParams) {
     <ActionModule
       background={1}
       title={dictionary('title')}
-      headerAction={<Link href={`/explorer/${nodeId}`}>{dictionary('viewOnExplorer')}</Link>}
+      headerAction={
+        <Link href={`/explorer/${nodeId}`}>
+          <span className="text-session-green fill-session-green inline-flex items-center gap-1 align-middle">
+            {dictionary('viewOnExplorer')}
+            <LinkOutIcon className="h-4 w-4" />
+          </span>
+        </Link>
+      }
     >
       <NodeStaking nodeId={nodeId} />
     </ActionModule>
