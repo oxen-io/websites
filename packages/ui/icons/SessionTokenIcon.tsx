21a4855aadb051c6fb3291f53e045b9791df4981
Ryan Miller <ryan.m@getsession.org>
2024-07-01 16:13:01 +1000

    feat: add session token icon

---
+++ packages/ui/icons/SessionTokenIcon.tsx
@@ -0,0 +1,124 @@
+import { forwardRef } from 'react';
+import { SVGAttributes } from './types';
+
+export const SessionTokenIcon = forwardRef<SVGSVGElement, SVGAttributes>((props, ref) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="Layer 1"
+    viewBox="0 0 373 400"
+    {...props}
+    ref={ref}
+  >
+    <defs>
+      <linearGradient
+        id="b"
+        x1="28.5"
+        x2="347.4"
+        y1="20.9"
+        y2="199.3"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#00fa9c" />
+        <stop offset=".3" stop-color="#00f491" />
+        <stop offset=".8" stop-color="#00e476" />
+        <stop offset="1" stop-color="#00dc67" />
+      </linearGradient>
+      <linearGradient
+        id="c"
+        x1="279.5"
+        x2="281.3"
+        y1="152.5"
+        y2="218.1"
+        gradientTransform="matrix(2.5 0 0 2.7 -486.7 -293.9)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".5" stop-color="#00f2cf" stop-opacity="0" />
+        <stop offset="1" stop-opacity=".4" />
+      </linearGradient>
+      <linearGradient
+        id="d"
+        x1="279.5"
+        x2="281.3"
+        y1="121.2"
+        y2="186.9"
+        gradientTransform="matrix(1.3 0 0 2.5 -37.4 -268.6)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".5" stop-color="#00f2cf" stop-opacity="0" />
+        <stop offset="1" stop-opacity=".4" />
+      </linearGradient>
+      <linearGradient
+        id="f"
+        x1="57.2"
+        x2="359.1"
+        y1="208.4"
+        y2="414.2"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#00f6d2" />
+        <stop offset=".4" stop-color="#00eec7" />
+        <stop offset="1" stop-color="#00e0b4" />
+      </linearGradient>
+      <linearGradient
+        id="g"
+        x1="362.4"
+        x2="364.2"
+        y1="90.9"
+        y2="156.6"
+        gradientTransform="matrix(2.8 0 0 3.3 -967.7 -199.1)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".5" stop-color="#00f2cf" stop-opacity="0" />
+        <stop offset="1" stop-opacity=".4" />
+      </linearGradient>
+      <linearGradient
+        id="h"
+        x1="191.8"
+        x2="193.6"
+        y1="193.7"
+        y2="259.4"
+        gradientTransform="matrix(2.6 0 0 2.6 -348.3 -479.8)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".5" stop-color="#00f2cf" stop-opacity="0" />
+        <stop offset="1" stop-opacity=".5" />
+      </linearGradient>
+      <clipPath id="a">
+        <path
+          d="M186.5 272.4c-23.8 0-47.6-9.1-65.7-27.2l-94.9-94.8C8.9 133.4-.3 110.8 0 86.8c.4-23.4 10-45.4 27.2-61.7C44.2 8.9 66.3 0 89.7 0h193.6c23.4 0 45.6 8.9 62.4 25a86.7 86.7 0 0 1 27.2 61.7c.4 24-8.8 46.7-25.8 63.7l-94.8 94.8a92.74 92.74 0 0 1-65.8 27.2M89.7 41.7c-26 0-47.5 20.5-47.9 45.7-.2 12.7 4.6 24.6 13.6 33.6l94.9 94.8c10 10 23.1 15 36.3 15s26.3-5 36.3-15l94.8-94.8c9-9 13.8-20.9 13.6-33.6-.4-25.2-21.9-45.7-47.9-45.7H89.7"
+          className="cls-4"
+        />
+      </clipPath>
+      <clipPath id="e">
+        <path
+          d="m347.1 249.6-94.9-94.8c-18.1-18.1-41.9-27.2-65.7-27.2s-47.6 9.1-65.8 27.2L105 170.5l29.5 29.5 15.7-15.7c10-10 23.1-15 36.3-15s26.3 5 36.3 15l94.9 94.8c9 9 13.8 20.9 13.6 33.6-.4 25.2-21.9 45.7-47.9 45.7H89.7c-26 0-47.6-20.5-47.9-45.7-.2-12.7 4.6-24.6 13.6-33.6l49.6-49.6L75.5 200l-49.6 49.6A87.8 87.8 0 0 0 0 313.3c.3 23.4 10 45.4 27.2 61.7 16.9 16.1 39.1 25 62.4 25h193.6a90 90 0 0 0 62.4-25 86.7 86.7 0 0 0 27.2-61.7 87.3 87.3 0 0 0-25.8-63.7Z"
+          className="cls-4"
+        />
+      </clipPath>
+    </defs>
+    <g clip-path="url(#a)">
+      <path
+        fill="url(#b)"
+        d="M186.5 272.4c-23.8 0-47.6-9.1-65.7-27.2l-94.9-94.8C8.9 133.4-.3 110.8 0 86.8c.4-23.4 10-45.4 27.2-61.7C44.2 8.9 66.3 0 89.7 0h193.6c23.4 0 45.6 8.9 62.4 25a86.7 86.7 0 0 1 27.2 61.7c.4 24-8.8 46.7-25.8 63.7l-94.8 94.8a92.74 92.74 0 0 1-65.8 27.2M89.7 41.7c-26 0-47.5 20.5-47.9 45.7-.2 12.7 4.6 24.6 13.6 33.6l94.9 94.8c10 10 23.1 15 36.3 15s26.3-5 36.3-15l94.8-94.8c9-9 13.8-20.9 13.6-33.6-.4-25.2-21.9-45.7-47.9-45.7H89.7"
+      />
+      <path
+        fill="url(#c)"
+        d="M158.2 190.1h122.4v97.3H158.2z"
+        transform="rotate(-120 219.34 238.7)"
+      />
+      <path fill="url(#d)" d="M284.4 107.5h61.1v91.2h-61.1z" transform="rotate(45 315 153.07)" />
+    </g>
+    <g clip-path="url(#e)">
+      <path
+        fill="url(#f)"
+        d="m347.1 249.6-94.9-94.8c-18.1-18.1-41.9-27.2-65.7-27.2s-47.6 9.1-65.8 27.2L105 170.5l29.5 29.5 15.7-15.7c10-10 23.1-15 36.3-15s26.3 5 36.3 15l94.9 94.8c9 9 13.8 20.9 13.6 33.6-.4 25.2-21.9 45.7-47.9 45.7H89.7c-26 0-47.6-20.5-47.9-45.7-.2-12.7 4.6-24.6 13.6-33.6l49.6-49.6L75.5 200l-49.6 49.6A87.8 87.8 0 0 0 0 313.3c.3 23.4 10 45.4 27.2 61.7 16.9 16.1 39.1 25 62.4 25h193.6a90 90 0 0 0 62.4-25 86.7 86.7 0 0 0 27.2-61.7 87.3 87.3 0 0 0-25.8-63.7Z"
+      />
+      <path
+        fill="url(#g)"
+        d="M-16.6 194.3h136.4v119.3H-16.6z"
+        transform="rotate(-135 51.54 253.97)"
+      />
+      <path fill="url(#h)" d="M89.9 112.9h126.3V210H89.9z" transform="rotate(60 153.06 161.5)" />
+    </g>
+  </svg>
+));
