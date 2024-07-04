a66441890c6dd8e7465d44c82aa9a3aad53f0a19
Ryan Miller <ryan.m@getsession.org>
2024-07-04 15:21:34 +1000

    fix: test real button

--- packages/auth/components/TelegramAuthButton.tsx
+++ packages/auth/components/TelegramAuthButton.tsx
@@ -1,30 +1,24 @@
 'use client';
 
-import { Button } from '@session/ui/ui/button';
 import { LoginButton } from '@telegram-auth/react';
-import { forwardRef, useRef } from 'react';
-import { TelegramIcon } from '../icons/TelegramIcon';
+import { forwardRef } from 'react';
 import { signIn, signOut, useSession } from '../lib/client';
-import { ButtonDataTestId } from '../testing/data-test-ids';
 
 type TelegramAuthButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   csrfToken: string;
 };
 
 export const TelegramAuthButton = forwardRef<HTMLButtonElement, TelegramAuthButtonProps>(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ({ csrfToken, ...props }, ref) => {
     const { data, status } = useSession();
-    const buttonContainerRef = useRef<HTMLDivElement>(null);
 
     const isConnected = status === 'authenticated';
     const username = data?.user?.name;
 
     console.log(data);
 
-    const handleButtonClick = () => {
-      const button = buttonContainerRef.current?.querySelector('button');
-      button?.click();
-    };
+    console.log('username', username);
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleClick = (data: any) => {
@@ -38,10 +32,10 @@ export const TelegramAuthButton = forwardRef<HTMLButtonElement, TelegramAuthButt
     return (
       <>
         <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-        <div className="hidden">
-          <LoginButton botUsername="session_testnet_bot" onAuthCallback={handleClick} />
-        </div>
-        <Button
+        {/* <div className="hidden"> */}
+        <LoginButton botUsername="session_testnet_bot" onAuthCallback={handleClick} />
+        {/* </div> */}
+        {/* <Button
           onClick={handleButtonClick}
           data-testid={ButtonDataTestId.TELEGRAM_AUTH}
           ref={ref}
@@ -49,7 +43,7 @@ export const TelegramAuthButton = forwardRef<HTMLButtonElement, TelegramAuthButt
         >
           <TelegramIcon className="h-4 w-4" />
           {isConnected ? username ?? 'Connected' : 'Connect Telegram'}
-        </Button>
+        </Button> */}
       </>
     );
   }
