702e33b8acc4b34a0c7694adbe66e38a8eaa1d69
Ryan Miller <ryan.m@getsession.org>
2024-07-04 15:16:26 +1000

    fix: add hidden button click

--- packages/auth/components/TelegramAuthButton.tsx
+++ packages/auth/components/TelegramAuthButton.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@session/ui/ui/button';
 import { LoginButton } from '@telegram-auth/react';
-import { forwardRef } from 'react';
+import { forwardRef, useRef } from 'react';
 import { TelegramIcon } from '../icons/TelegramIcon';
 import { signIn, signOut, useSession } from '../lib/client';
 import { ButtonDataTestId } from '../testing/data-test-ids';
@@ -14,16 +14,22 @@ type TelegramAuthButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 export const TelegramAuthButton = forwardRef<HTMLButtonElement, TelegramAuthButtonProps>(
   ({ csrfToken, ...props }, ref) => {
     const { data, status } = useSession();
+    const buttonContainerRef = useRef<HTMLDivElement>(null);
 
     const isConnected = status === 'authenticated';
     const username = data?.user?.name;
 
     console.log(data);
 
+    const handleButtonClick = () => {
+      const button = buttonContainerRef.current?.querySelector('button');
+      button?.click();
+    };
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleClick = (data: any) => {
       if (!isConnected) {
-        signIn('telegram', { callbackUrl: window.location.href }, data);
+        signIn('telegram', {}, data);
       } else {
         signOut();
       }
@@ -33,10 +39,10 @@ export const TelegramAuthButton = forwardRef<HTMLButtonElement, TelegramAuthButt
       <>
         <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
         <div className="hidden">
-          <LoginButton botUsername="session_testnet_bot" />
+          <LoginButton botUsername="session_testnet_bot" onAuthCallback={handleClick} />
         </div>
         <Button
-          onClick={handleClick}
+          onClick={handleButtonClick}
           data-testid={ButtonDataTestId.TELEGRAM_AUTH}
           ref={ref}
           {...props}
