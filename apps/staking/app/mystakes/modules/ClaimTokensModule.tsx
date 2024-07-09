007f7bcd97bdae97415d3154812eaca9b5abe7b0
Ryan Miller <ryan.m@getsession.org>
2024-07-09 18:33:24 +1000

    fix: disable claim button

--- apps/staking/app/mystakes/modules/ClaimTokensModule.tsx
+++ apps/staking/app/mystakes/modules/ClaimTokensModule.tsx
@@ -5,15 +5,10 @@ import { PresentIcon } from '@session/ui/icons/PresentIcon';
 
 export default function ClaimTokensModule() {
   return (
-    <Module
-      className="group items-center"
-      onClick={() => {
-        console.log('clicked claim');
-      }}
-    >
-      <ModuleContent className="flex h-full select-none flex-row items-center gap-2 align-middle font-bold">
-        <PresentIcon className="fill-session-text group-hover:fill-session-black h-6 w-6" />
-        <ModuleText className="h-8 text-3xl group-hover:text-black">Claim</ModuleText>
+    <Module className="items-center">
+      <ModuleContent className="flex h-full select-none flex-row items-center gap-2 align-middle font-bold hover:bg-inherit">
+        <PresentIcon className="fill-session-text group-hover:fill-session-black h-6 w-6 opacity-50" />
+        <ModuleText className="h-8 text-3xl opacity-50 group-hover:text-black">Claim</ModuleText>
       </ModuleContent>
     </Module>
   );
