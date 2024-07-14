df08f266495ef4f55b7ddcf67f9b257d0f3ca0a0
Ryan Miller <ryan.m@getsession.org>
2024-07-15 09:18:02 +1000

    fix: move providers into GlobalProvider

--- apps/staking/app/layout.tsx
+++ apps/staking/app/layout.tsx
@@ -1,22 +1,17 @@
-import { NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID } from '@/lib/env';
+import { isProduction, NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID } from '@/lib/env';
 import { getLocalizationData } from '@/lib/locale-server';
 import { Toaster } from '@session/ui/components/ui/sonner';
 import { AtypDisplay, AtypText, MonumentExtended } from '@session/ui/fonts';
 
 import { DevSheet } from '@/components/DevSheet';
 import { siteMetadata, wagmiMetadata } from '@/lib/metadata';
-import FeatureFlagProvider from '@/providers/feature-flag-provider';
-import LocalizationProvider from '@/providers/localization-provider';
 import '@session/ui/styles';
-import { isProduction } from '@session/util/env';
 import { createWagmiConfig } from '@session/wallet/lib/wagmi';
-import { WalletButtonProvider } from '@session/wallet/providers/wallet-button-provider';
-import { Web3ModalProvider } from '@session/wallet/providers/web3-modal-provider';
 import { headers } from 'next/headers';
 import { cookieToInitialState } from 'wagmi';
-import ChainBanner from '../components/ChainBanner';
-import Header from '../components/Header';
-import SentStakingClientProvider from '../providers/sent-staking-provider';
+import ChainBanner from '@/components/ChainBanner';
+import Header from '@/components/Header';
+import { GlobalProvider } from '@/providers/global-provider';
 
 const wagmiConfig = createWagmiConfig({
   projectId: NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
@@ -37,27 +32,20 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       dir={direction}
       className={`${AtypDisplay.variable} ${AtypText.variable} ${MonumentExtended.variable}`}
     >
-      <FeatureFlagProvider>
-        <LocalizationProvider messages={messages} locale={locale}>
-          <WalletButtonProvider>
-            <Web3ModalProvider
-              initialState={initialWagmiState}
-              wagmiMetadata={wagmiMetadata}
-              projectId={NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
-            >
-              <SentStakingClientProvider>
-                <body className="bg-session-black text-session-text font-atyp-text overflow-x-hidden">
-                  <ChainBanner />
-                  <Header />
-                  <main>{children}</main>
-                  <Toaster />
-                  {!isProduction() ? <DevSheet /> : null}
-                </body>
-              </SentStakingClientProvider>
-            </Web3ModalProvider>
-          </WalletButtonProvider>
-        </LocalizationProvider>
-      </FeatureFlagProvider>
+      <GlobalProvider
+        messages={messages}
+        locale={locale}
+        initialState={initialWagmiState}
+        wagmiMetadata={wagmiMetadata}
+      >
+        <body className="bg-session-black text-session-text font-atyp-text overflow-x-hidden">
+          <ChainBanner />
+          <Header />
+          <main>{children}</main>
+          <Toaster />
+          {!isProduction ? <DevSheet /> : null}
+        </body>
+      </GlobalProvider>
     </html>
   );
 }
---
+++ apps/staking/providers/global-provider.tsx
@@ -0,0 +1,42 @@
+import { NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID } from '@/lib/env';
+import FeatureFlagProvider from '@/providers/feature-flag-provider';
+import LocalizationProvider, { LocalizationProviderProps } from '@/providers/localization-provider';
+import '@session/ui/styles';
+import { WalletButtonProvider } from '@session/wallet/providers/wallet-button-provider';
+import {
+  Web3ModalProvider,
+  Web3ModalProviderProps,
+} from '@session/wallet/providers/web3-modal-provider';
+import QueryProvider from '@/providers/query-provider';
+import { ReactNode } from 'react';
+
+type GlobalProviderParams = Omit<Web3ModalProviderProps, 'projectId'> &
+  Pick<LocalizationProviderProps, 'locale' | 'messages'> & {
+    children: ReactNode;
+  };
+
+export async function GlobalProvider({
+  children,
+  wagmiMetadata,
+  initialState,
+  messages,
+  locale,
+}: GlobalProviderParams) {
+  return (
+    <QueryProvider>
+      <FeatureFlagProvider>
+        <LocalizationProvider messages={messages} locale={locale}>
+          <WalletButtonProvider>
+            <Web3ModalProvider
+              initialState={initialState}
+              wagmiMetadata={wagmiMetadata}
+              projectId={NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
+            >
+              {children}
+            </Web3ModalProvider>
+          </WalletButtonProvider>
+        </LocalizationProvider>
+      </FeatureFlagProvider>
+    </QueryProvider>
+  );
+}
