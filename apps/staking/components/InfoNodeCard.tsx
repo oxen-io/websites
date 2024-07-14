f52f847782c97ef9c3dcb3de3574020c1024aba5
Ryan Miller <ryan.m@getsession.org>
2024-07-15 09:36:32 +1000

    feat: migrate open node logic to info node card

---
+++ apps/staking/components/InfoNodeCard.tsx
@@ -0,0 +1,105 @@
+import { forwardRef, type HTMLAttributes, type ReactNode } from 'react';
+import { ButtonDataTestId } from '@/testing/data-test-ids';
+import { useTranslations } from 'next-intl';
+import { NodeCard, NodeCardText, NodeCardTitle } from '@/components/NodeCard';
+import { cn } from '@session/ui/lib/utils';
+import { StatusIndicator } from '@session/ui/components/StatusIndicator';
+import { PubKey } from '@/components/PubKey';
+import Link from 'next/link';
+import { Button, ButtonSkeleton } from '@session/ui/ui/button';
+import { Skeleton } from '@session/ui/ui/skeleton';
+import { TextSeparator } from '@session/ui/components/Separator';
+
+export type InfoNodeCardProps = HTMLAttributes<HTMLDivElement> & {
+  pubKey: string;
+  button?: {
+    text: string;
+    link: string;
+    dataTestId: ButtonDataTestId;
+    ariaLabel: string;
+  };
+};
+
+export const InfoNodeCard = forwardRef<HTMLDivElement, InfoNodeCardProps>(
+  ({ className, pubKey, button, children, ...props }, ref) => {
+    const generalNodeDictionary = useTranslations('sessionNodes.general');
+    const titleFormat = useTranslations('modules.title');
+    return (
+      <NodeCard
+        ref={ref}
+        {...props}
+        className="flex flex-col items-center justify-between gap-2 align-middle sm:flex-row md:gap-10"
+      >
+        <div className={cn('text-center sm:text-start', className)}>
+          <div className="flex w-full cursor-pointer items-baseline gap-3 text-center align-middle sm:text-start">
+            <div className="-mr-2 scale-75 p-0 sm:mr-0 md:scale-100 md:p-0.5">
+              <StatusIndicator status="green" />
+            </div>
+            <NodeCardTitle className="inline-flex flex-wrap gap-2 text-sm md:text-lg">
+              <span className="text-nowrap font-normal">
+                {titleFormat('format', { title: generalNodeDictionary('publicKeyShort') })}
+              </span>
+              <PubKey pubKey={pubKey} force="collapse" />
+            </NodeCardTitle>
+          </div>
+          <NodeCardText className="col-span-10 mt-1 inline-flex max-h-max flex-row-reverse justify-center gap-2 text-center align-middle text-xs font-normal sm:justify-start sm:text-start md:mt-0 md:flex-row md:text-base">
+            {children}
+          </NodeCardText>
+        </div>
+        {button ? (
+          <Link href={button.link} className="w-full sm:w-auto" prefetch>
+            <Button
+              variant="outline"
+              size="md"
+              rounded="md"
+              aria-label={button.ariaLabel}
+              data-testid={button.dataTestId}
+              className="w-full sm:w-auto"
+            >
+              {button.text}
+            </Button>
+          </Link>
+        ) : null}
+      </NodeCard>
+    );
+  }
+);
+
+export function InfoNodeCardSkeleton() {
+  return (
+    <div className="border-muted flex w-full flex-row items-center justify-between gap-3 rounded-xl border-2 p-6">
+      <div className="-bottom-1/2 flex w-full flex-col gap-3">
+        <div className="flex w-full items-center gap-3 align-middle">
+          <div className="-mr-2 scale-75 p-0 sm:mr-0 md:scale-100 md:p-0.5">
+            <Skeleton className="h-4 w-4 rounded-full" />
+          </div>
+          <div className="inline-flex gap-2">
+            <Skeleton className="h-6 w-40 sm:w-60" />
+          </div>
+        </div>
+        <Skeleton className="h-4 w-9/12" />
+      </div>
+      <ButtonSkeleton variant="outline" rounded="md" className="h-8 w-16" />
+    </div>
+  );
+}
+
+export const NodeItem = ({ children, className }: { children: ReactNode; className?: string }) => (
+  <span className={className}>{children}</span>
+);
+
+export const NodeItemSeparator = ({ className }: { className?: string }) => (
+  <TextSeparator className={className} />
+);
+
+export const NodeItemLabel = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => <span className={cn('font-normal', className)}> {children}</span>;
+
+export const NodeItemValue = ({ children }: { children: ReactNode }) => (
+  <span className="text-nowrap font-semibold"> {children}</span>
+);
--- apps/staking/components/OpenNodeCard.tsx
+++ apps/staking/components/OpenNodeCard.tsx
@@ -5,32 +5,16 @@ import { ButtonDataTestId } from '@/testing/data-test-ids';
 import { SENT_SYMBOL } from '@session/contracts';
 import type { OpenNode } from '@session/sent-staking-js/client';
 import { generateMinAndMaxContribution } from '@session/sent-staking-js/test';
-import { TextSeparator } from '@session/ui/components/Separator';
-import { StatusIndicator } from '@session/ui/components/StatusIndicator';
-import { cn } from '@session/ui/lib/utils';
-import { Button } from '@session/ui/ui/button';
 import { formatNumber } from '@session/util/maths';
 import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import { forwardRef, useMemo, type HTMLAttributes, type ReactNode } from 'react';
-import { NodeCard, NodeCardText, NodeCardTitle } from './NodeCard';
-import { PubKey } from './PubKey';
-
-const NodeItem = ({ children, className }: { children: ReactNode; className?: string }) => (
-  <span className={className}>{children}</span>
-);
-
-const NodeItemSeparator = ({ className }: { className?: string }) => (
-  <TextSeparator className={className} />
-);
-
-const NodeItemLabel = ({ children, className }: { children: ReactNode; className?: string }) => (
-  <span className={cn('font-normal', className)}> {children}</span>
-);
-
-const NodeItemValue = ({ children }: { children: ReactNode }) => (
-  <span className="text-nowrap font-semibold"> {children}</span>
-);
+import { forwardRef, type HTMLAttributes, useMemo } from 'react';
+import {
+  InfoNodeCard,
+  NodeItem,
+  NodeItemLabel,
+  NodeItemSeparator,
+  NodeItemValue,
+} from '@/components/InfoNodeCard';
 
 const OpenNodeCard = forwardRef<
   HTMLDivElement,
@@ -38,12 +22,11 @@ const OpenNodeCard = forwardRef<
 >(({ className, node, ...props }, ref) => {
   const dictionary = useTranslations('nodeCard.open');
   const generalNodeDictionary = useTranslations('sessionNodes.general');
-  const stakingNodeDictionary = useTranslations('sessionNodes.staking');
   const titleFormat = useTranslations('modules.title');
 
   const { service_node_pubkey: pubKey, fee } = node;
 
-  const [formattedMinContributon, formattedMaxContribution] = useMemo(() => {
+  const [formattedMinContribution, formattedMaxContribution] = useMemo(() => {
     const { minContribution, maxContribution } = generateMinAndMaxContribution({
       contributors: node.contributions,
     });
@@ -52,65 +35,45 @@ const OpenNodeCard = forwardRef<
   }, [node.contributions]);
 
   return (
-    <NodeCard
+    <InfoNodeCard
       ref={ref}
+      className={className}
+      pubKey={pubKey}
+      button={{
+        ariaLabel: dictionary('viewButton.ariaLabel'),
+        text: dictionary('viewButton.text'),
+        dataTestId: ButtonDataTestId.Node_Card_View,
+        link: `/stake/${pubKey}`,
+      }}
       {...props}
-      className="flex flex-col items-center justify-between gap-2 align-middle sm:flex-row md:gap-10"
     >
-      <div className={cn('text-center sm:text-start', className)}>
-        <div className="flex w-full cursor-pointer items-baseline gap-3 text-center align-middle sm:text-start">
-          <div className="-mr-2 scale-75 p-0 sm:mr-0 md:scale-100 md:p-0.5">
-            <StatusIndicator status="green" />
-          </div>
-          <NodeCardTitle className="inline-flex flex-wrap gap-2 text-sm md:text-lg">
-            <span className="text-nowrap font-normal">
-              {titleFormat('format', { title: generalNodeDictionary('publicKeyShort') })}
-            </span>
-            <PubKey pubKey={pubKey} force="collapse" />
-          </NodeCardTitle>
-        </div>
-        <NodeCardText className="col-span-10 mt-1 inline-flex max-h-max flex-row-reverse justify-center gap-2 text-center align-middle text-xs font-normal sm:justify-start sm:text-start md:mt-0 md:flex-row md:text-base">
-          <NodeItem>
-            <NodeItemLabel className="inline-flex md:hidden">
-              {titleFormat('format', { title: dictionary('min') })}
-            </NodeItemLabel>
-            <NodeItemLabel className="hidden md:inline-flex">
-              {titleFormat('format', { title: dictionary('minContribution') })}
-            </NodeItemLabel>
-            <NodeItemValue>
-              {formattedMinContributon} {SENT_SYMBOL}
-            </NodeItemValue>
-          </NodeItem>
-          <NodeItemSeparator className="hidden md:block" />
-          <NodeItem className="hidden md:block">
-            <NodeItemLabel>{titleFormat('format', { title: dictionary('max') })}</NodeItemLabel>
-            <NodeItemValue>
-              {formattedMaxContribution} {SENT_SYMBOL}
-            </NodeItemValue>
-          </NodeItem>
-          <NodeItemSeparator />
-          <NodeItem>
-            <NodeItemLabel>
-              {titleFormat('format', { title: generalNodeDictionary('operatorFeeShort') })}
-            </NodeItemLabel>
-            {/** TODO: replace this */}
-            <NodeItemValue>{formatPercentage(fee / 10000)}</NodeItemValue>
-          </NodeItem>
-        </NodeCardText>
-      </div>
-      <Link href={`/stake/node/${pubKey}`} className="w-full sm:w-auto">
-        <Button
-          variant="outline"
-          size="md"
-          rounded="md"
-          aria-label={dictionary('viewButton.ariaLabel')}
-          data-testid={ButtonDataTestId.Node_Card_View}
-          className="w-full sm:w-auto"
-        >
-          {dictionary('viewButton.text')}
-        </Button>
-      </Link>
-    </NodeCard>
+      <NodeItem>
+        <NodeItemLabel className="inline-flex md:hidden">
+          {titleFormat('format', { title: dictionary('min') })}
+        </NodeItemLabel>
+        <NodeItemLabel className="hidden md:inline-flex">
+          {titleFormat('format', { title: dictionary('minContribution') })}
+        </NodeItemLabel>
+        <NodeItemValue>
+          {formattedMinContribution} {SENT_SYMBOL}
+        </NodeItemValue>
+      </NodeItem>
+      <NodeItemSeparator className="hidden md:block" />
+      <NodeItem className="hidden md:block">
+        <NodeItemLabel>{titleFormat('format', { title: dictionary('max') })}</NodeItemLabel>
+        <NodeItemValue>
+          {formattedMaxContribution} {SENT_SYMBOL}
+        </NodeItemValue>
+      </NodeItem>
+      <NodeItemSeparator />
+      <NodeItem>
+        <NodeItemLabel>
+          {titleFormat('format', { title: generalNodeDictionary('operatorFeeShort') })}
+        </NodeItemLabel>
+        {/** TODO: replace this */}
+        <NodeItemValue>{formatPercentage(fee / 10000)}</NodeItemValue>
+      </NodeItem>
+    </InfoNodeCard>
   );
 });
 OpenNodeCard.displayName = 'OpenNodeCard';
