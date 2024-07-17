b6ea262aa1ad2a5c0ed26dc6675fbe2cf58b4726
Ryan Miller <ryan.m@getsession.org>
2024-07-17 14:37:19 +1000

    fix: getPendingNodes now filters out already registered nodes

--- apps/staking/lib/queries/getPendingNodes.ts
+++ apps/staking/lib/queries/getPendingNodes.ts
@@ -10,6 +10,8 @@ export async function getPendingNodes(
   ]);
 
   if (registrationsResponse.data.registrations.length && stakedNodesResponse.data.nodes.length) {
+    console.log('reg', registrationsResponse);
+    console.log('stake', stakedNodesResponse);
     const stakedNodeIds = stakedNodesResponse.data.nodes.map((node) => node.pubkey_ed25519);
     const pendingRegistrations = registrationsResponse.data.registrations.filter(
       (registration) => !stakedNodeIds.includes(registration.pubkey_ed25519)
