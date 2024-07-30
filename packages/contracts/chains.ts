51a0ecc9a29c5e5b595796c348af26371817afa1
Ryan Miller <ryan.m@getsession.org>
2024-07-30 11:57:22 +1000

    feat: create CHAIN to chainId map

--- packages/contracts/chains.ts
+++ packages/contracts/chains.ts
@@ -27,6 +27,14 @@ export const chains = {
   [CHAIN.ETHEREUM]: ethereum,
 } as const;
 
+export const chainForId = {
+  [mainnet.id]: CHAIN.MAINNET,
+  [testnet.id]: CHAIN.TESTNET,
+  [ethereum.id]: CHAIN.ETHEREUM,
+} as const;
+
+export type AllowedChainId = keyof typeof chainForId;
+
 export type ChainData = keyof typeof chains;
 
 /**
