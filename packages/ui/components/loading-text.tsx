871c98a0dd33fe4690f85ed29b09b4a8fb19a01e
Ryan Miller <ryan.m@getsession.org>
2024-08-12 15:21:26 +1000

    fix: make LoadingText className optional

--- packages/ui/components/loading-text.tsx
+++ packages/ui/components/loading-text.tsx
@@ -1,7 +1,7 @@
 import './loading-text.css';
 import { cn } from '../lib/utils';
 
-export const LoadingText = ({ className }: { className: string }) => {
+export const LoadingText = ({ className }: { className?: string }) => {
   return (
     <div className={cn('lds-ellipsis', className)}>
       <div />
