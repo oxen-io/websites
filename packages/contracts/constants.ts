26041bb700288b5eac6aae0022030b51582746f3
Ryan Miller <ryan.m@getsession.org>
2024-08-22 10:08:16 +1000

    feat: upgrade all contract details to use v2

--- packages/contracts/constants.ts
+++ packages/contracts/constants.ts
@@ -13,6 +13,33 @@ export type ContractName = (typeof contracts)[number];
 export const addresses: Record<ContractName, Record<CHAIN, Address>> = {
   SENT: {
     /** @deprecated - The Mainnet value is a mock value */
+    [CHAIN.MAINNET]: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+    /** @deprecated - The Eth value is a mock value */
+    [CHAIN.ETHEREUM]: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+    [CHAIN.TESTNET]: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+  },
+  ServiceNodeRewards: {
+    /** @deprecated - The Mainnet value is a mock value */
+    [CHAIN.MAINNET]: '0xb691e7C159369475D0a3d4694639ae0144c7bAB2',
+    /** @deprecated - The Eth value is a mock value */
+    [CHAIN.ETHEREUM]: '0xb691e7C159369475D0a3d4694639ae0144c7bAB2',
+    [CHAIN.TESTNET]: '0xb691e7C159369475D0a3d4694639ae0144c7bAB2',
+  },
+  RewardRatePool: {
+    /** @deprecated - The Mainnet value is a mock value */
+    [CHAIN.MAINNET]: '0x84a648F74Eaf037dD9558987F6179E692d5F2566',
+    /** @deprecated - The Eth value is a mock value */
+    [CHAIN.ETHEREUM]: '0x84a648F74Eaf037dD9558987F6179E692d5F2566',
+    [CHAIN.TESTNET]: '0x84a648F74Eaf037dD9558987F6179E692d5F2566',
+  },
+} as const;
+
+/**
+ * @deprecated - Use {@link addresses} instead
+ */
+export const addressesV1: Record<ContractName, Record<CHAIN, Address>> = {
+  SENT: {
+    /** @deprecated - The Mainnet value is a mock value */
     [CHAIN.MAINNET]: '0x7FBDC29b81e410eB0eaE75Dca64a76d898EAc4A9',
     /** @deprecated - The Eth value is a mock value */
     [CHAIN.ETHEREUM]: '0x7FBDC29b81e410eB0eaE75Dca64a76d898EAc4A9',
