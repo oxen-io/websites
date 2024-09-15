939555a9199b358d49f521d311a70dffdcfdc79c
Ryan Miller <aerilym@pm.me>
2024-09-16 09:05:15 +1000

    fix: tailwind spelling mistake

--- packages/ui/components/ui/tooltip.tsx
+++ packages/ui/components/ui/tooltip.tsx
@@ -31,7 +31,7 @@ const TooltipContent = forwardRef<
     sideOffset={sideOffset}
     side="top"
     className={cn(
-      'text-session-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-session-black border-px s m:max-w-lg z-50 max-w-[90svw] overflow-hidden rounded-xl border border-[#1C2624] bg-opacity-50 px-4 py-2 text-sm shadow-xl outline-none',
+      'text-session-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-session-black border-px z-50 max-w-[90svw] overflow-hidden rounded-xl border border-[#1C2624] bg-opacity-50 px-4 py-2 text-sm shadow-xl outline-none sm:max-w-lg',
       className
     )}
     {...props}
