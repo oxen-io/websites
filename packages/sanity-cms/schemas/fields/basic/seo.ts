4b62a15856ce910b22279241a40f1d56721f35ae
Ryan Miller <aerilym@pm.me>
2024-10-02 09:42:36 +1000

    feat: add SEO sanity type

--- packages/sanity-cms/schemas/fields/basic/seo.ts
+++ packages/sanity-cms/schemas/fields/basic/seo.ts
@@ -3,17 +3,82 @@ import { defineField } from 'sanity';
 export const seoField = defineField({
   title: 'SEO',
   name: 'seo',
+  description: 'SEO fields for the content',
   type: 'seoMetaFields',
+  group: 'seo',
 });
 
-export const seoPreview = {
-  select: {
-    metaTitle: 'seo',
-  },
-  prepare(selection: { metaTitle?: string }) {
-    const metaTitle = selection?.metaTitle || '';
-    return {
-      title: metaTitle || 'seo',
+export type SeoType = {
+  _type?: 'seo';
+  nofollowAttributes?: boolean;
+  metaDescription?: string;
+  additionalMetaTags?: MetaTagType[];
+  metaTitle?: string;
+  seoKeywords?: string[];
+  openGraph?: OpenGraphType;
+  twitter?: Twitter;
+};
+
+export type MetaTagType = {
+  _type: 'metaTag';
+  metaAttributes?: MetaAttributeType[];
+};
+
+export type MetaAttributeType = {
+  _type: 'metaAttribute';
+  attributeKey?: string;
+  attributeType?: string;
+  attributeValueString?: string;
+  attributeValueImage?: CustomImageType;
+};
+
+export type OpenGraphType = {
+  _type: 'openGraph';
+  title: string;
+  url?: string;
+  siteName?: string;
+  description: string;
+  image: CustomImageType;
+};
+
+export type Twitter = {
+  _type: 'twitter';
+  handle?: string;
+  creator?: string;
+  site?: string;
+  cardType?: string;
+};
+
+export type CustomImageType = {
+  _type: 'customImage';
+  asset?: SanityImageAssetType;
+  crop?: {
+    _type: 'SanityImageCrop';
+    right: number;
+    top: number;
+    left: number;
+    bottom: number;
+  };
+  hotspot?: {
+    x: number;
+    y: number;
+    height: number;
+    _type: 'SanityImageHotspot';
+    width?: number;
+  };
+};
+
+export type SanityImageAssetType = {
+  _type?: 'SanityImageAsset';
+  _id?: string;
+  path?: string;
+  url?: string;
+  metadata?: {
+    _type?: 'SanityImageMetadata';
+    dimensions?: {
+      _type?: 'SanityImageDimensions';
+      height?: number;
+      width?: number;
     };
-  },
+  };
 };
