17b699e70aa3ced5343526a806f32a22e49ddd18
Ryan Miller <aerilym@pm.me>
2024-10-02 19:47:54 +1000

    feat: add custom button component to portable text schema

--- packages/sanity-cms/schemas/fields/generated/copy.ts
+++ packages/sanity-cms/schemas/fields/generated/copy.ts
@@ -1,6 +1,24 @@
 import { defineArrayMember } from 'sanity';
+import { SplitVerticalIcon } from '@sanity/icons';
+import { pickLinkField } from '../basic/links';
 
 export const copyFieldOf = [
   defineArrayMember({ name: 'block', type: 'block' }),
   defineArrayMember({ name: 'image', type: 'image' }),
+  defineArrayMember({
+    name: 'button',
+    type: 'object',
+    fields: [pickLinkField],
+    icon: SplitVerticalIcon,
+    // TODO: figure out how to make this work
+    // marks: {
+    //   decorators: [
+    //     {
+    //       title: 'Button',
+    //       value: 'button',
+    //       component: SanityButton,
+    //     },
+    //   ],
+    // },
+  }),
 ];
