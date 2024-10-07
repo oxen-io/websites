2ea840e82e76c75a31904a4ce341df41f4b10536
Ryan Miller <aerilym@pm.me>
2024-10-07 11:59:22 +1100

    chore: create sanity utility libraries

--- packages/sanity-cms/lib/util.ts
+++ packages/sanity-cms/lib/util.ts
@@ -1,23 +1,36 @@
 import { safeTrySync } from '@session/util-js/try';
 import { draftMode } from 'next/headers';
 import logger from './logger';
+import { isProduction } from '@session/util-js/env';
 
 /**
  * Checks if draft mode is enabled.
  *
  * @link https://nextjs.org/docs/app/api-reference/functions/draft-mode#checking-if-draft-mode-is-enabled
  *
- * @param isClient - If the function is being called from the client
  * @returns If draft mode is enabled
  */
-export const isDraftModeEnabled = (isClient = false) => {
-  if (isClient) return false;
+export const isDraftModeEnabled = () => {
+  const [err, result] = safeTrySync(draftMode);
 
-  const [error, result] = safeTrySync(draftMode);
+  if (err) {
+    /**
+     * If the error is a "called outside a request scope" error, it means the function was called
+     * outside a request scope, so we can draft mode is not enabled. This happens when cms calls are
+     * made during SSR.
+     */
+    if ('message' in err && err.message.includes('called outside a request scope')) {
+      return false;
+    }
 
-  if (error) {
-    logger.error(`Error getting draft mode`, error);
-    return false;
+    logger.error(`Error getting draft mode`);
+
+    if (isProduction()) {
+      logger.error(err);
+      return false;
+    }
+
+    throw err;
   }
 
   return result.isEnabled;
