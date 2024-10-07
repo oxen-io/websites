de7d9927dcce49ace4b0793d8757cb956997a969
Ryan Miller <aerilym@pm.me>
2024-10-07 11:58:35 +1100

    fix: rework sanity preview draft mode

--- packages/sanity-cms/components/SanityLayout.tsx
+++ packages/sanity-cms/components/SanityLayout.tsx
@@ -1,38 +1,24 @@
 import { type ReactNode, Suspense } from 'react';
 import { isDraftModeEnabled } from '../lib/util';
-import type { SessionSanityClient } from '../lib/client';
-import PreviewProvider from '../providers/PreviewProvider';
 import { VisualEditing } from 'next-sanity';
+import SanityDisableDraftMode from './SanityDisableDraftMode';
 
 export default async function SanityLayout({
-  client,
-  token,
   children,
+  disableDraftModePath,
 }: {
-  client: SessionSanityClient;
-  token: string;
   children: ReactNode;
+  disableDraftModePath: string;
 }) {
   const isDraftMode = isDraftModeEnabled();
   return (
     <>
-      {isDraftMode && (
-        <a
-          className="fixed bottom-0 right-0 m-4 bg-blue-500 p-4 text-white"
-          href="/api/draft-mode/disable"
-        >
-          Disable preview mode
-        </a>
-      )}
       {isDraftMode ? (
-        <Suspense fallback={children}>
-          <PreviewProvider token={token} client={client}>
-            {children}
-          </PreviewProvider>
+        <Suspense>
+          <SanityDisableDraftMode disableDraftModePath={disableDraftModePath} />
         </Suspense>
-      ) : (
-        <>{children}</>
-      )}
+      ) : null}
+      {children}
       {isDraftMode ? <VisualEditing /> : null}
     </>
   );
