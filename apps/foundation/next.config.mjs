c85cfe288e078445bff3d51ed2635ecb7601ed1d
Ryan Miller <aerilym@pm.me>
2024-10-14 16:16:35 +1100

    chore: add next config env

--- apps/foundation/next.config.mjs
+++ apps/foundation/next.config.mjs
@@ -5,6 +5,13 @@ const withNextIntl = createNextIntlPlugin('./lib/locale-server.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    SANITY_API_READ_TOKEN: process.env.SANITY_API_READ_TOKEN,
+    SANITY_REVALIDATE_SECRET: process.env.SANITY_REVALIDATE_SECRET,
+  },
   transpilePackages: [
     '@session/ui',
     '@session/util-js',
