ec009ac7674c1c637903b040cb6e11d082d557b2
Ryan Miller <aerilym@pm.me>
2024-10-16 09:01:30 +1100

    fix: blog post alignment on macOS Sonoma

--- apps/foundation/app/(Site)/blog/[slug]/BlogPost.tsx
+++ apps/foundation/app/(Site)/blog/[slug]/BlogPost.tsx
@@ -37,7 +37,7 @@ export default async function BlogPost({ post }: PostProps) {
     .filter(Boolean);
 
   return (
-    <article className="mx-auto mb-32 mt-4 flex max-w-screen-xl flex-col items-start">
+    <article className="mb-32 mt-4 flex max-w-screen-xl flex-col items-start">
       <Link href={SANITY_SCHEMA_URL.POST} prefetch>
         <Button
           data-testid={ButtonDataTestId.Back_To_Blog}
@@ -51,12 +51,12 @@ export default async function BlogPost({ post }: PostProps) {
         </Button>
       </Link>
       <PostInfoBlock
-        className="w-full"
+        className="h-max w-full"
         postInfo={post}
         renderWithPriority
         mobileImagePosition="below"
       />
-      <div className="mt-6 flex flex-row justify-center gap-12 md:mt-12">
+      <div className="mt-6 flex h-max flex-row justify-center gap-12 md:mt-12">
         <PortableText
           body={body}
           className={cn(headings.length && 'max-w-screen-md')}
